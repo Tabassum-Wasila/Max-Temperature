cf9712553aa788315802d8cf85005200aa6fc5aa
Tabassum-Wasila <twahsila@gmail.com>
2017-09-19 15:10:36 +0600

    new methods added to find the hottest day in a year

--- src/CSVMax.java
+++ src/CSVMax.java
@@ -1,5 +1,6 @@
 import edu.duke.*;
 import org.apache.commons.csv.*;
+import java.io.*;
 public class CSVMax {
 	public CSVRecord hottestHourInDay(CSVParser parser)
 	{
@@ -22,6 +23,36 @@ public class CSVMax {
 	{
 		FileResource fr = new FileResource("C://Users/Tabassum Wasila Sus/Downloads/nc_weather/nc_weather/2012/weather-2012-01-01.csv");
 		CSVRecord largest = hottestHourInDay(fr.getCSVParser());
-		System.out.println("hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimeEST"));
+	}
+	public CSVRecord hottestInYear()
+	{
+		DirectoryResource dr = new DirectoryResource();
+		CSVRecord largestSoFar = null;
+		for(File f: dr.selectedFiles())
+		{
+			FileResource fr = new FileResource(f);
+			CSVRecord current = hottestHourInDay(fr.getCSVParser());
+			if(largestSoFar == null)
+				largestSoFar = current;
+			else
+			{
+				double currentTemp = Double.parseDouble(current.get("TemperatureF"));
+				double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+				if(currentTemp > largestTemp)
+					largestSoFar = current;
+			}
+			
+		}
+		return largestSoFar;
+	}
+	public void testHottestDayInYear()
+	{
+		CSVRecord largest = hottestInYear();
+		System.out.println("Hottest Temperature was " + largest.get("TemperatureF") + " at " + largest.get("DateUTC"));
+	}
+	public static void main(String[] args)
+	{
+		CSVMax c = new CSVMax();
+		c.testHottestDayInYear();
 	}
 }
